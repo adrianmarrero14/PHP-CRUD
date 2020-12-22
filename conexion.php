0e7ec5680d71ef12779069476a31b6420b0e4765
Adrian <adrianmarrero18@gmail.com>
2020-12-22 13:36:04 -0400

    AWS

--- conexion.php
+++ conexion.php
@@ -1,11 +1,18 @@
 <?php
-$link = 'mysql:host=127.0.0.1:3307;dbname=yt_colores';
-$usuario = 'root';
-$pass = '';
+
+$dbhost = '************************'; // CONSULTAR CON ADRIÁN
+$dbport = '3306';
+$dbname = 'sefardata';
+$charset = 'utf8';
+
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+$usuario = '******';                 // CONSULTAR CON ADRIÁN
+$pass = '*****';                     // CONSULTAR CON ADRIÁN
 
 try {
-    $pdo = new PDO($link, $usuario, $pass);
+    $pdo = new PDO($dsn, $usuario, $pass);
 } catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
 }
+
--- index.php
+++ index.php
@@ -3,36 +3,13 @@
 include_once 'conexion.php';
 
 //  LEER
-$sql_leer = 'SELECT * FROM colores';
+$sql_leer = 'SELECT * FROM sefardata.paises';
 $gsent = $pdo->prepare($sql_leer);
 $gsent->execute();
 $resultado = $gsent->fetchAll();
 
-//var_dump($resultado);
+// var_dump($resultado);
 
-//  AGREGAR
-if ($_POST) {
-    $color = $_POST['color'];
-    $descripcion = $_POST['descripcion'];
-
-    $sql_agregar = 'INSERT INTO colores (color,descripcion) VALUES (?,?)';
-    $sentencia_agregar = $pdo->prepare($sql_agregar);
-    $sentencia_agregar->execute(array($color, $descripcion));
-
-    $sentencia_agregar = null;
-    $pdo = null;
-
-    header('location:index.php');
-}
-
-//EDITAR 
-if ($_GET) {
-    $id = $_GET['id'];
-    $sql_unico = 'SELECT * FROM colores WHERE id=?';
-    $gsent_unico = $pdo->prepare($sql_unico);
-    $gsent_unico->execute(array($id));
-    $resultado_unico = $gsent_unico->fetch();
-}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -42,54 +19,20 @@ if ($_GET) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP CRUD</title>
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 
 <body>
-
     <div class="container mt-5">
         <div class="row">
-            <!-- DIV COLORES -->
+            <!-- DIV PAISES -->
             <div class="col-md-6">
                 <?php foreach ($resultado as $dato) : ?>
-                    <div class="alert alert-<?php echo $dato['color'] ?> text-uppercase" role="alert">
-                        <?php echo $dato['color'] ?>
-                        -
-                        <?php echo $dato['descripcion'] ?>
-
-                        <a href="eliminar.php?id=<?php echo $dato['id'] ?>" class="float-right ml-3">
-                            <i class="fas fa-trash-alt"></i>
-                        </a>
-
-                        <a href="index.php?id=<?php echo $dato['id'] ?>" class="float-right">
-                            <i class="fas fa-edit"></i>
-                        </a>
+                    <div class="alert alert-primary text-uppercase" role="alert">
+                        <?php echo $dato['Pais'] ?>
                     </div>
                 <?php endforeach ?>
-            </div>
-
-            <!-- DIV FORM -->
-            <div class="col-md-6">
-                <?php if (!$_GET) : ?>
-                    <h2>Agregar Elementos</h2>
-                    <form method="POST">
-                        <input type="text" name="color" id="" class="form-control mb-3">
-                        <input type="text" name="descripcion" id="" class="form-control mb-3">
-                        <button class="btn btn-primary">Agregar</button>
-                    </form>
-                <?php endif ?>
-
-                <?php if ($_GET) : ?>
-                    <h2>Editar Elemento</h2>
-                    <form method="GET" action="editar.php">
-                        <input type="text" name="color" id="" class="form-control mb-3" value="<?php echo $resultado_unico['color'] ?>">
-                        <input type="text" name="descripcion" id="" class="form-control mb-3" value="<?php echo $resultado_unico['descripcion'] ?>">
-                        <input type="hidden" name="id" value="<?php echo $resultado_unico['id'] ?>">
-                        <button class="btn btn-warning">Editar</button>
-                    </form>
-                <?php endif ?>
             </div>
         </div>
     </div>
